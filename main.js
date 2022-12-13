e70bc84ece2ef4ae0095f9d10265fd1758b2843a
ElvDev05 <vasquezelverth@gmail.com>
2022-12-12 22:05:38 -0500

    proyecto acabado

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-const activate = document.querySelector('.header__sidebar--img');
+const activate = document.querySelector('.header__menu-logo');
 activate.addEventListener('click', function(){
 document.getElementById('sidebar').classList.toggle('activate--sidebar');
+});
+const desactivate=document.querySelector('.header__sidebar--img');
+desactivate.addEventListener('click', function(){
+    document.getElementById('sidebar').classList.remove('activate--sidebar');
 });
