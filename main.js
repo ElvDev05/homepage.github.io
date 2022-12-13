bc59d9eab1c60e2285f557b7c145de3b651e6e43
ElvDev05 <vasquezelverth@gmail.com>
2022-12-12 21:38:52 -0500

    agregando JavaScript

---
+++ main.js
@@ -0,0 +1,4 @@
+const activate = document.querySelector('.header__sidebar--img');
+activate.addEventListener('click', function(){
+document.getElementById('sidebar').classList.toggle('activate--sidebar');
+});
